e9736ce1d5f33a58ac605b9dd4009fa722ed7b3a
Vladimir Neizhmakov <vovka_x@mail.ru>
2020-04-05 02:31:41 +0300

    * Реализовать добавление новых кружков по клику используя ТОЛЬКО массивы  ** Реализовать по клику другой кнопки удаление кружков (никаких эррейЛист)

--- src/lesson_1/MainCircles.java
+++ src/lesson_1/MainCircles.java
@@ -2,6 +2,8 @@ package lesson_1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainCircles extends JFrame {
     private static final int POS_X = 400;
@@ -10,6 +12,15 @@ public class MainCircles extends JFrame {
     private static final int WINDOW_HEIGHT = 600;
 
     Sprite[] sprites = new Sprite[10];
+
+    private Sprite[] arrayLengthChange(Sprite[] sprites, int newLength) {
+        if (newLength>=0) {
+            Sprite[] arr = new Sprite[newLength];
+            System.arraycopy(sprites, 0, arr, 0, sprites.length < newLength ? sprites.length : newLength);
+            return arr;
+        }
+        return sprites;
+    }
 
     public static void main(String[] args) {
        SwingUtilities.invokeLater(new Runnable() {
@@ -28,6 +39,24 @@ public class MainCircles extends JFrame {
         setTitle("Circles");
         initApplication();
         setVisible(true);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                onMouseClick(e);
+            }
+        });
+    }
+
+    private void onMouseClick(MouseEvent e)  {
+        if (e.getButton() == 1) {
+            sprites = arrayLengthChange(sprites,sprites.length + 1);
+            sprites[sprites.length-1] = new Ball();
+
+        }
+        if (e.getButton() == 3 || e.getButton() == 2) {
+            sprites = arrayLengthChange(sprites,sprites.length - 1);
+        }
     }
 
     private void initApplication() {
