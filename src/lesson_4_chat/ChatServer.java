26d154ef1c309f468199069882d5a42fc4d5a6fd
Vladimir Neizhmakov <vovka_x@mail.ru>
2020-04-16 12:02:22 +0300

    Исходник урока №4

---
+++ src/lesson_4_chat/ChatServer.java
@@ -0,0 +1,12 @@
+package lesson_4_chat;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+
+}
